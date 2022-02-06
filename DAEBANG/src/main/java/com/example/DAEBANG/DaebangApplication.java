85aff45581b3ed36a3eaf8fd14d101d4c2e5a903
hj5o <dataliteracy@naver.com>
2022-02-06 19:34:45 +0900

    Initial commit

---
+++ DAEBANG/src/main/java/com/example/DAEBANG/DaebangApplication.java
@@ -0,0 +1,13 @@
+package com.example.DAEBANG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaebangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaebangApplication.class, args);
+	}
+
+}
---
+++ DAEBANG/src/test/java/com/example/DAEBANG/DaebangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DAEBANG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaebangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
